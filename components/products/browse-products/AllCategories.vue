e82ce68803bb0a28623f1fa06e77733554ab91c0
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-09-12 15:35:51 +0300

    add Components AllCategories ProductCard
    Add apollo queries for use in the components

---
+++ components/products/browse-products/AllCategories.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur rerum
+    possimus cum, perspiciatis expedita quibusdam iste aspernatur. Cumque qui
+    repudiandae eum est?
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+// import { Product, Category } from '~/types/types'
+// @ts-ignore
+import Categories from '~/apollo/queries/categories'
+
+export default Vue.extend({
+  apollo: {
+    categories: {
+      query: Categories,
+      prefetch: true,
+      variables() {
+        return {
+          pagination: {
+            start: 1,
+            first: 6,
+          },
+        }
+      },
+    },
+  },
+})
+</script>
