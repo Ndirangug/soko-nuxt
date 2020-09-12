b9a9e2e5057b1407378bdf391cad7007f02cb90f
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-09-12 12:05:18 +0300

    Code cleanup
    add 'all_categories' to the locale messages object for convenience

--- pages/_lang/products/index.vue
+++ pages/_lang/products/index.vue
@@ -3,7 +3,6 @@
     class="root-container"
     @toggle-navdrawer="onToggleNavDrawer"
     @category-does-not-exist="onCategoryDoesNotExist"
-    @category-locale-not-found="onCategoryLocaleNotFound"
   >
     <!-- #####   top banner section of page   #####-->
     <BannerAndToolbar :category="category.replace('_', '-')" />
@@ -34,14 +33,10 @@ export default Vue.extend({
 
   computed: {
     availableCategories(): string[] {
-      return [
-        ...Object.keys(this.$t('navigation.rooms')),
-        ...Object.keys(this.$t('navigation.specifics')),
-        ...Object.keys(this.$t('navigation.other_categories')),
-      ]
+      return [...Object.keys(this.$t('all_categories'))]
     },
     category(): string {
-      const category = this.$route.query.category
+      const queryCategory = this.$route.query.category
         ? this.$route.query.category
             .toString()
             .trim()
@@ -50,28 +45,10 @@ export default Vue.extend({
             .replace('-', '_')
         : 'null'
 
-      // todo clean this up asap
-      if (this.availableCategories.includes(category)) {
-        if (!this.$t(`navigation.rooms.${category}`).toString().includes('.')) {
-          return this.$t(`navigation.rooms.${category}`).toString()
-        } else if (
-          !this.$t(`navigation.specifics.${category}`).toString().includes('.')
-        ) {
-          return this.$t(`navigation.specifics.${category}`).toString()
-        } else if (
-          !this.$t(`navigation.other_categories.${category}`)
-            .toString()
-            .includes('.')
-        ) {
-          return this.$t(`navigation.other_categories.${category}`).toString()
-        } else {
-          this.$emit('category-locale-not-found', category)
-          return this.$t(
-            'products.browse_products.browse_categories'
-          ).toString()
-        }
+      if (this.availableCategories.includes(queryCategory)) {
+        return this.$t(`all_categories.${queryCategory}`).toString()
       } else {
-        this.$emit('category-does-not-exist', category)
+        this.$emit('category-does-not-exist', queryCategory)
         return this.$t('products.browse_products.browse_categories').toString()
       }
     },
@@ -82,9 +59,6 @@ export default Vue.extend({
       this.drawerState = drawerState
     },
     onCategoryDoesNotExist(e: string) {
-      console.log(e)
-    },
-    onCategoryLocaleNotFound(e: string) {
       console.log(e)
     },
   },
