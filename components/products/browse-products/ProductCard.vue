f58f9c94de184e5c0caae7711da6227f098a3844
George Ndirangu <ndirangu.mepawa@gmail.com>
2020-09-15 14:15:08 +0300

    feat: Add height width

--- components/products/browse-products/ProductCard.vue
+++ components/products/browse-products/ProductCard.vue
@@ -1,9 +1,8 @@
 <template>
-  <div>
-    <v-card class="mx-4" tile>
-      <client-only>
-        <v-img :src="product.thumbnails[0]" />
-      </client-only>
+  <div class="card-container">
+    <v-card class="mx-4 my-4 card" tile>
+      <v-img :src="product.thumbnails[0]" />
+
       <v-card-title>{{ product.title }}</v-card-title>
       <v-card-subtitle>{{ product.price }}</v-card-subtitle>
     </v-card>
@@ -23,3 +22,25 @@ export default Vue.extend({
   },
 })
 </script>
+
+<style lang="scss" scoped>
+.card-container {
+  width: 20vw;
+  height: 41vw;
+  min-width: 200px;
+  overflow: hidden;
+
+  .card {
+    overflow: hidden;
+    width: 100%;
+    height: 95%;
+  }
+}
+
+// phones <  screen <= tablets
+@media only screen and (max-width: $tablet_max_px) and (min-width: $phone_max_px) {
+}
+// screen <= phone
+@media screen and (max-width: $phone_max_px) {
+}
+</style>
